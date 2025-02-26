554e4ecffc2ce9ccb8226f1ee6c98fe15a12b36d
haha <3109944373@qq.com>
2025-02-26 22:33:13 +0800

    feat: sync command page

---
+++ packages/server/src/types/apis/sync.ts
@@ -0,0 +1,11 @@
+/**
+ * Sync params
+ */
+export interface SyncParams {
+
+}
+
+/**
+ * Sync command details
+ */
+export interface SyncDetails { totalChats?: number, totalFolders?: number, processedChats?: number, processedFolders?: number }
